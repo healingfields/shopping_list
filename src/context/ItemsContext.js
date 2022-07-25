54c00720ddfa2ee3d5c56db78894a7b06bd127ec
healingfields <dihratene@gmail.com>
2022-07-25 12:14:49 +0100

    created a function in the ItemsContext.js for adding items to a certain list

--- src/context/ItemsContext.js
+++ src/context/ItemsContext.js
@@ -1,5 +1,5 @@
-import {createContext, useCallback, useReducer, useState} from "react";
-import useDataFetching from "../hooks/useDataFetching";
+import {createContext, useCallback, useReducer} from "react";
+
 
 export const ItemsContext = createContext();
 
@@ -25,11 +25,16 @@ const reducer = (state, action) => {
                 error:action.payload,
                 loading:false
             };
+        case 'ADD_ITEM':
+            return{
+                ...state,
+                items:[...state.items, action.payload],
+                loading:false
+            };
         default:
             return state;
     }
 }
-
 
 export const ItemsContextProvider = ({children}) => {
 
@@ -48,9 +53,44 @@ export const ItemsContextProvider = ({children}) => {
         }
     }, [])
 
+    const addItem = useCallback(async ({listId, title, quantity, price})=>{
+
+        const itemId = Math.floor(Math.random() * 100);
+        try{
+            const data = await fetch(
+                'https://my-json-\n' +
+                'server.typicode.com/PacktPublishing/React-\n' +
+                'Projects-Second-Edition/items',
+                {
+                    method:'POST',
+                    body:JSON.stringify({
+                        id:itemId,
+                        listId,
+                        title,
+                        quantity,
+                        price,
+                    })
+                }
+            )
+            const result = await data.json();
+
+            if(result){
+                dispatch({type:'ADD_ITEM', payload:{
+                        id:itemId,
+                        listId,
+                        title,
+                        quantity,
+                        price
+                    }});
+            }
+        }catch (e) {
+            console.log(e.message)
+        }
+    },[])
+
 
     return(
-        <ItemsContext.Provider value={{...state, fetchItems}}>
+        <ItemsContext.Provider value={{...state, fetchItems, addItem}}>
             {children}
         </ItemsContext.Provider>
      )
